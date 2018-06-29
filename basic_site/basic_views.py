05b042ac919b849f017ddcc2c120b7a8085768d5
steven athouel <sathouel@gmail.com>
2018-06-29 14:10:26 +0300

    main pages added, routed, debugged

--- basic_site/basic_views.py
+++ basic_site/basic_views.py
@@ -1,7 +1,4 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
-from werkzeug.exceptions import abort
+from flask import Blueprint, render_template
 
 
 bp = Blueprint('bv', __name__)
@@ -10,3 +7,13 @@ bp = Blueprint('bv', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/about')
+def about():
+    return render_template('about_us.html')
+
+
+@bp.route('/contact')
+def contact():
+    return render_template('contact_us.html')
